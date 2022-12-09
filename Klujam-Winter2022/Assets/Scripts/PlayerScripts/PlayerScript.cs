d8a1f39514e3500e4c52e225851d8439fc7c384a
Mohamed Medhat <55616007+Medhatt12@users.noreply.github.com>
2022-12-09 18:55:00 +0100

    added behaviour

---
+++ Klujam-Winter2022/Assets/Scripts/PlayerScripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class PlayerSc : MonoBehaviour
+{
+    [SerializeField]
+    public int points = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Debug.Log(points.ToString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
